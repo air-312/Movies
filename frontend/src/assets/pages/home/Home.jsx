e01eb930a5a146e72d00f5ce199fdd2dd9c5e1f6
Frédérick <ahobautfrederick@gmail.com>
2024-12-22 15:25:48 +0100

    premiere section terminé

--- frontend/src/assets/pages/home/Home.jsx
+++ frontend/src/assets/pages/home/Home.jsx
@@ -3,8 +3,33 @@ import './Home.css'
 const Home =()=>{
     return (
         <>
-            <div className="container-home">
-                
+            <div className=" text-light container-home">
+                <div className="section-one-custom d-flex flex-column align-items-center justify-content-center">
+                    <div className="text-center d-flex  align-items-center td-custom">
+                        <h4 className='fs-custom'>Films en illimité</h4>
+                        <p>
+                            Partir de 5,99 €. Annulable à tout moment.
+                            <br/>
+                            Découvrez une vaste sélection de films, disponibles à tout moment. Profitez de l&apos;expérience cinématographique ultime, où que vous soyez.
+                        </p>
+                        
+                        <form action="" className='d-flex align-items-center justify-content-center bg-light custom'>
+                        <input 
+                            type="email" 
+                            name="" 
+                            id="" 
+                            placeholder='Adresse e-mail'
+                            className='border-0 bg-light input-custom'
+                        />
+                            <button className='py-1 px-3 d-flex border-0 btn-custom' type="submit">Commencer<i className='fas fa-chevron-right'></i></button>
+                    </form>
+                    </div>
+                    
+                </div>
+                <div className="section-two">
+                    <div className="top"></div>
+
+                </div>
             </div>
         </>
     )
