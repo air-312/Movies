2e5e56c6ac8cb84e753f7bfaac78b2da7ef47eaa
Frédérick <ahobautfrederick@gmail.com>
2024-12-22 01:23:24 +0100

    footer terminé

--- frontend/src/assets/components/footer/Footer.jsx
+++ frontend/src/assets/components/footer/Footer.jsx
@@ -1,48 +1,44 @@
 import{Link} from 'react-router-dom'
+import Logo from '../../images/Logos/Logo_movies_ft.svg'
 import './Footer.css'
 
 const Footer = () =>{
     return (
-        <footer className='bg-lith'>
-            <div className="container-footer">
-                <div className="row">
-                    <div className="footer-col">
-                        <h4>A propos de <span>Movies</span></h4>
-                        <p>
+        <footer className='text-light custom-container-footer'>
+            <div className="w-25">
+                        <Link to="/"><img className='logo' src={Logo} alt="" /></Link>
+                        <p className='p-custom'>
                         Movies est votre plateforme incontournable pour découvrir et profiter de vos films préférés. Plongez dans notre vaste bibliothèque et vivez le cinéma comme jamais auparavant.              </p>
-                    </div>
-                    <div className="footer-col">
-                        <h4>Liens rapides</h4>
-                        <ul>
+            </div>
+            <div className="footer-col">
+                        <h4 className='fs-2 text-light fw-bold mb-5 custom'>Liens rapides</h4>
+                        <ul className='list-unstyled'>
                             <li>
-                                <Link>Accueil</Link>
+                                <Link className='fs-5 text-light d-block custom-link-menu'>Accueil</Link>
                             </li>
                             <li>
-                                <Link>Populaire</Link>
+                                <Link className='fs-5 text-light d-block custom-link-menu'>Populaire</Link>
                             </li>
                             <li>
-                                <Link>Mieux notés</Link>
+                                <Link className='fs-5 text-light d-block custom-link-menu'>Mieux notés</Link>
                             </li>
                             <li>
-                                <Link>Se connecter</Link>
+                                <Link className='fs-5 text-light d-block custom-link-menu'>Se connecter</Link>
                             </li>
                             <li>
-                                <Link>Contact</Link>
+                                <Link className='fs-5 text-light d-block custom-link-menu'>Contact</Link>
                             </li>
                         </ul>
-                    </div>
-                    <div className="footer-col">
-                        <h4>Suivez-nous</h4>
-                        <div className="social-links">
-                            <Link><i className="fab fa-facebook-f"></i></Link>
-                            <Link><i className="fab fa-twitter"></i></Link>
-                            <Link><i className="fab fa-instagram"></i></Link>
-                            <Link><i className="fab fa-youtube"></i></Link>
-                        </div>
-                    </div>
-                </div>
             </div>
-        
+            <div className="footer-col">
+                        <h4 className='fs-2 text-light fw-bold mb-5 custom'>Suivez-nous</h4>
+                        <div className="d-flex g-custom">
+                            <Link className='text-center text-dark   custom-social-link'><i className="fab fa-facebook-f"></i></Link>
+                            <Link className='text-center text-dark  custom-social-link'><i className="fab fa-twitter"></i></Link>
+                            <Link className='text-center text-dark  custom-social-link'><i className="fab fa-instagram"></i></Link>
+                            <Link className='text-center text-dark  custom-social-link'><i className="fab fa-youtube"></i></Link>
+                        </div>
+            </div>
         </footer>
     )
 }
